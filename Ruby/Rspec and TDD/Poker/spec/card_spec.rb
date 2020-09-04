62f7b42dda9e3d6f749113e5496099b7b948824d
Michael Stockermans <michael.stockermans@gmail.com>
2020-09-04 13:11:14 -0400

    Poker Card spec

--- Ruby/Rspec and TDD/Poker/spec/card_spec.rb
+++ Ruby/Rspec and TDD/Poker/spec/card_spec.rb
@@ -1,11 +1,23 @@
 require 'card'
 
 describe Card do
+  subject(:card) { Card.new("J", "D")}
+
   describe '#initialize' do
-    it 'holds the suit and value of a card'
-    it 'should raise an error for an invalid suit'
-    it 'should raise an error for an invalid value'
-    it 'should be readable for suit'
-    it 'should be readable for value'
-  end
+    it 'holds the suit and value of a card' do
+      expect(card.suit).to eq("D")
+      expect(card.value).to eq("J")
+    end
+
+    it 'should raise an error for an invalid suit' do
+      expect { Card.new("J", "Z") }.to raise_error("Card: Invalid suit.")
+      expect { Card.new("J", 3) }.to raise_error("Card: Invalid suit.")
+    end
+
+    it 'should raise an error for an invalid value' do
+      expect { Card.new(-1, "H") }.to raise_error("Card: Invalid value.")
+      expect { Card.new(0, "D") }.to raise_error("Card: Invalid value.")
+      expect { Card.new(11, "C") }.to raise_error("Card: Invalid value.")
+      expect { Card.new(12, "S") }.to raise_error("Card: Invalid value.")
+    end
 end
