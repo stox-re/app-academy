27b107df9e49670ed929163062a2c3a548c434c4
Michael Stockermans <michael.stockermans@gmail.com>
2020-09-11 10:36:07 -0400

    Hash Map & LRU Cache p01_int_set.rb

---
+++ Ruby/LRU Cache/Hash Map & LRU Cache/lib/p01_int_set.rb
@@ -0,0 +1,118 @@
+class MaxIntSet
+  attr_reader :store
+
+  def initialize(max)
+    @max = max
+    @store = Array.new(max, false)
+  end
+
+  def insert(num)
+    raise "Out of bounds" if !is_valid?(num)
+    @store[num] = true
+  end
+
+  def remove(num)
+    raise "Out of bounds" if !is_valid?(num)
+    @store[num] = false
+  end
+
+  def include?(num)
+    raise "Out of bounds" if !is_valid?(num)
+    @store[num]
+  end
+
+  private
+
+  def is_valid?(num)
+    num >= 0 && num <= @store.length - 1
+  end
+end
+
+class IntSet
+  attr_reader :store
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+    self[num] << num if !include?(num)
+  end
+
+  def remove(num)
+    self[num].delete(num)
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    @store[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count, :store
+  BUCKET_RESIZE = 2
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+    if !include?(num)
+      self[num] << num
+      @count += 1
+      resize! if @count >= num_buckets
+      return true
+    end
+    false
+  end
+
+  def remove(num)
+    if self[num].include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    @store[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+    puts "Calling resize"
+    @count = 0
+    previous_buckets = @store.dup
+    @store = Array.new(num_buckets * BUCKET_RESIZE) { Array.new }
+
+    previous_buckets.each do |sub_array|
+      sub_array.each do |ele|
+        insert(ele)
+      end
+    end
+  end
+end
+
+
+ resizing = ResizingIntSet.new(20)
+ 21.times { |i| resizing.insert(i)}
+ p resizing.store.length
