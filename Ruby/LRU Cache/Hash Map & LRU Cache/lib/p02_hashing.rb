3c6996303d4beb16d2302f2621725c9256551938
Michael Stockermans <michael.stockermans@gmail.com>
2020-09-11 11:36:15 -0400

    p02_hashing.rb

--- Ruby/LRU Cache/Hash Map & LRU Cache/lib/p02_hashing.rb
+++ Ruby/LRU Cache/Hash Map & LRU Cache/lib/p02_hashing.rb
@@ -4,11 +4,19 @@ end
 
 class Array
   def hash
+    self.each_with_index.inject(0) do |hash, (num, index)|
+      (num.hash + index.hash)
+    end
   end
 end
 
 class String
   def hash
+    char_int_array = []
+    self.each_char do |char|
+      char_int_array << char.ord
+    end
+    char_int_array.hash
   end
 end
 
@@ -16,6 +24,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    array = Array.new
+    self.each do |key, value|
+      array << [key, value]
+    end
+    array.sort.hash
   end
 end
