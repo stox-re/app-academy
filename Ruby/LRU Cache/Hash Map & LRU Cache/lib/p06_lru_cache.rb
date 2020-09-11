bed419e26edac678dc3282a9d9ba6dfbac613103
Michael Stockermans <michael.stockermans@gmail.com>
2020-09-11 12:41:18 -0400

    LRU Cache p06_lru_cache.rb

--- Ruby/LRU Cache/Hash Map & LRU Cache/lib/p06_lru_cache.rb
+++ Ruby/LRU Cache/Hash Map & LRU Cache/lib/p06_lru_cache.rb
@@ -14,6 +14,17 @@ class LRUCache
   end
 
   def get(key)
+    if @map.include?(key)
+      node = @map.get(key)
+      update_node!(node)
+      node.val
+    else
+      eject! if count == @max
+      value = @prc.call(key)
+      node = @store.append(key, value)
+      @map[key] = node
+      value
+    end
   end
 
   def to_s
@@ -22,14 +33,14 @@ class LRUCache
 
   private
 
-  def calc!(key)
-    # suggested helper method; insert an (un-cached) key
-  end
-
   def update_node!(node)
-    # suggested helper method; move a node to the end of the list
+    node.remove
+    @map[node.key] = @store.append(node.key, node.val)
   end
 
   def eject!
+    node =  @store.first
+    @map.delete(node.key)
+    node.remove
   end
 end
