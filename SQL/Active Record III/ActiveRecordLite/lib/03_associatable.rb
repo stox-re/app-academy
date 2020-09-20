5371b25ee9f7e1b7aa2f73eb6e03fe396337bf60
Michael Stockermans <michael.stockermans@gmail.com>
2020-09-20 14:41:37 -0400

    ActiveRecordLite: Associatable starting

--- SQL/Active Record III/ActiveRecordLite/lib/03_associatable.rb
+++ SQL/Active Record III/ActiveRecordLite/lib/03_associatable.rb
@@ -10,29 +10,45 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    self.class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    defaults = {
+      foreign_key: "#{name}_id".to_sym,
+      primary_key: :id,
+      class_name: "#{name}".capitalize
+    }
+
+    defaults.each do |key, value|
+      send("#{key}=", options[key] || value )
+    end
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    defaults = {
+      foreign_key: "#{self_class_name.downcase || name}_id".to_sym,
+      primary_key: :id,
+      class_name: name.capitalize.singularize
+    }
+    defaults.each do |key, value|
+      send("#{key}=", options[key] || value )
+    end
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
+
     # ...
   end
 
@@ -47,4 +63,5 @@ end
 
 class SQLObject
   # Mixin Associatable here...
+  extend Associatable
 end
