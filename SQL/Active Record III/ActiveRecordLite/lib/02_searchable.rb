dda72f9364ce7ab586d0ecbb60b24e59598ed0c8
Michael Stockermans <michael.stockermans@gmail.com>
2020-09-20 13:51:13 -0400

    ActiveRecordLite: Searchable Where

--- SQL/Active Record III/ActiveRecordLite/lib/02_searchable.rb
+++ SQL/Active Record III/ActiveRecordLite/lib/02_searchable.rb
@@ -3,10 +3,22 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_clause = []
+    params.each do |key, value|
+      where_clause << key.to_s + " = ?"
+    end
+    where_clause = where_clause.join(" AND ")
+
+    results = DBConnection.execute(<<-SQL, *params.values)
+      SELECT *
+      FROM #{table_name}
+      WHERE #{where_clause}
+    SQL
+
+    parse_all(results)
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
