87d2864aaf208417aade8f98c6b807e25651523a
Michael Stockermans <michael.stockermans@gmail.com>
2020-09-25 11:06:51 -0400

    Cats99: Cat model setup with age

---
+++ Rails/Rails Views/Cats99/app/models/cat.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint           not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Cat < ApplicationRecord
+  include ActionView::Helpers::DateHelper
+
+  validates :birth_date, presence: true
+  validates :color, presence: true, inclusion: { in: %w(black white grey red), message: "%{value} is not a valid color" }
+  validates :name, presence: true
+  validates :sex, presence: true, inclusion: { in: %w(M F), message: "%{value} is not a valid sex" }
+  validates :description, presence: true
+
+  def age
+    time_ago_in_words(self.birth_date.to_datetime)
+  end
+end
--- Rails/Rails Views/Cats99/db/seeds.rb
+++ Rails/Rails Views/Cats99/db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cat.destroy_all
+
+cat1 = Cat.create(
+  birth_date: Date.new(2015, 2, 3),
+  color: "grey",
+  name: "Tommy",
+  sex: "M",
+  description: "Tommy is a happy little cat who loves to play with feathers."
+)
+
+cat2 = Cat.create(
+  birth_date: Date.new(2018, 10, 15),
+  color: "grey",
+  name: "Sarah",
+  sex: "F",
+  description: "Sarah loves her scratching pole, and enjoys sleeping in the sun."
+)
