eecc58a1a59accc140c2a0bcc61835cee0079997
Michael Stockermans <michael.stockermans@gmail.com>
2020-09-28 12:17:10 -0400

    MusicApp: bands added with routes and views

---
+++ Rails/Rails Auth II/MusicApp/app/controllers/bands_controller.rb
@@ -0,0 +1,55 @@
+class BandsController < ApplicationController
+  def index
+    @bands = Band.order(:id)
+    render :index
+  end
+
+  def new
+    render :new
+  end
+
+  def create
+    new_band = Band.new(cat_rental_request_params)
+    if new_band.save!
+      redirect_to band_url(new_band.id)
+    else
+      flash[:errors] = @new_band.errors.full_messages
+      redirect_to new_band_url
+    end
+  end
+
+  def show
+    @band = Band.find_by({id: params[:id]})
+    render :show
+  end
+
+  def edit
+    @band = Band.find_by({id: params[:id]})
+    render :edit
+  end
+
+  def update
+    update_band = Band.find_by({id: params[:id]})
+    if update_band.update!(cat_rental_request_params)
+      redirect_to band_url(update_band.id)
+    else
+      flash[:errors] = update_band.errors.full_messages
+    end
+  end
+
+  def destroy
+    @band = Band.find_by(id: params[:id])
+    if @band.destroy!
+      redirect_to bands_url
+    else
+      flash[:errors] = @band.errors.full_messages
+    end
+
+  end
+
+  private
+
+  def cat_rental_request_params
+    params.require(:band).permit(:name)
+  end
+end
--- Rails/Rails Auth II/MusicApp/config/routes.rb
+++ Rails/Rails Auth II/MusicApp/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :bands, only: [:index, :create, :new, :edit, :show, :update, :destroy]
   resources :users, only: [:new, :create, :show]
   resource :session, only: [:create, :destroy, :new]
 
-  root to: redirect("/users/new")
+  root to: redirect("/bands")
 end
