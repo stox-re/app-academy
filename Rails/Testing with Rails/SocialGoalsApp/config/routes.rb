dd7d25a51df05f662e104f48de98d53bfec92d4d
Michael Stockermans <michael.stockermans@gmail.com>
2020-09-29 20:52:48 -0400

    SocialGoalsApp: passing auth specs

--- Rails/Testing with Rails/SocialGoalsApp/config/routes.rb
+++ Rails/Testing with Rails/SocialGoalsApp/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create, :show]
+  resource :session, only: [:create, :destroy, :new]
+
+  root to: redirect("/session/new")
 end
--- Rails/Testing with Rails/SocialGoalsApp/spec/features/auth_spec.rb
+++ Rails/Testing with Rails/SocialGoalsApp/spec/features/auth_spec.rb
@@ -8,18 +8,21 @@ feature 'the signup process' do
   end
 
   feature 'signing up a user' do
-    scenario 'shows username on the homepage after signup'
+    scenario 'shows username on the homepage after signup' do
       visit "/users/new"
       email = Faker::Internet.email
       fill_in "Email", with: email
       fill_in "Password", with: Faker::Internet.password(min_length: 6)
       click_button "Sign up"
       expect(page).to have_content(email)
+    end
   end
 end
 
 feature 'logging in' do
   scenario 'shows username on the homepage after login' do
+      user = User.new({email: "test@testing.com", password: "testing"})
+      user.save!
       visit "/session/new"
       fill_in "Email", with: "test@testing.com"
       fill_in "Password", with: "testing"
@@ -31,15 +34,18 @@ end
 feature 'logging out' do
   scenario 'begins with a logged out state' do
     visit "/"
-    expect(page).to have_content "Login"
+    expect(page).to have_button "Login"
   end
 
   scenario 'doesn\'t show username on the homepage after logout' do
+      user = User.new({email: "test@testing.com", password: "testing"})
+      user.save!
       visit "/session/new"
       fill_in "Email", with: 'test@testing.com'
       fill_in "Password", with: "testing"
       click_button "Login"
-      click_link "Logout"
+      # It redirects now to /users/1
+      click_button "Logout"
       expect(page).not_to have_content "test@testing.com"
   end
 end
