4f64c2352baf5af5e664e004f0fc54cf79e95f1d
Michael Stockermans <michael.stockermans@gmail.com>
2020-10-04 12:28:13 -0400

    RailsLite: session and controller_base

--- Rails/Rails Lite/Rails Lite/lib/controller_base.rb
+++ Rails/Rails Lite/Rails Lite/lib/controller_base.rb
@@ -23,6 +23,7 @@ class ControllerBase
       raise "Already built response" if @already_built_response
       @res.status = 302
       @res["Location"] = url
+      @session.store_session(@res)
       @already_built_response = true
   end
 
@@ -33,16 +34,24 @@ class ControllerBase
     raise "Already built response" if @already_built_response
     @res["Content-Type"] = content_type
     @res.write(content)
+    @session.store_session(@res)
     @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    controller_dir = ActiveSupport::Inflector.underscore(self.class.name)
+    path = "#{File.dirname(File.expand_path('..', __FILE__))}/views/#{controller_dir}/#{template_name}.html.erb"
+    template_file = File.open(path, "r")
+    reading_file = template_file.read
+    template = ERB.new(reading_file).result(binding)
+    render_content(template, "text/html")
   end
 
   # method exposing a `Session` object
   def session
+    @session ||= Session.new(@req)
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
