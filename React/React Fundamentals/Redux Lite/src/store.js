a8c77d46ad64b3e2911519721b8945326a3d71f0
Michael Stockermans <michael.stockermans@gmail.com>
2020-10-14 19:30:43 -0400

    Moved react folder

---
+++ React/React Fundamentals/Redux Lite/src/store.js
@@ -0,0 +1,11 @@
+class Store {
+
+  constructor(rootReducer) {
+    this.rootReducer = rootReducer;
+    this.globalState = {};
+  }
+
+  getState() {
+    return Object.assign({}, this.globalState);
+  }
+}
