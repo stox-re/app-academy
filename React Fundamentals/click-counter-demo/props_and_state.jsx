185d763aea3ec6c52f5559c08be0aa7ffb0af767
Michael Stockermans <michael.stockermans@gmail.com>
2020-10-14 17:52:33 -0400

    Props and state notes

---
+++ React Fundamentals/click-counter-demo/props_and_state.jsx
@@ -0,0 +1,33 @@
+// Don't change props, use state for that
+
+class WordInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      word: ""
+    };
+    this.updateWord = this.updateWord.bind(this);
+  }
+
+  updateWord(event) {
+    this.setState({ word: event.currentTarget.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <input onChange={this.updateWord} value={this.state.word} />
+        <span>The word is: {this.state.word}</span>
+      </div>
+    );
+  }
+}
+
+export default WordInput;
+
+/*
+  Note that because setState() triggers a re-render,
+  it should not be called during a render(),
+  as that would trigger an infinite loop.
+  Here, setState() works because this.updateWord is called by user-input after the element is rendered.
+*/
