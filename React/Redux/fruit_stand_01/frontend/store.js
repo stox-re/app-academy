bf345231d83c6b2a4eb8b7731f9870a65addce37
Michael Stockermans <michael.stockermans@gmail.com>
2020-10-17 14:25:30 -0400

    Fruit stand redux store demo

---
+++ React/Redux/fruit_stand_01/frontend/store.js
@@ -0,0 +1,19 @@
+import { createStore } from 'redux';
+import reducer from './reducer.js';
+import { addOrange, addApple } from './actions.js';
+
+const store = createStore(reducer); // instantiate app's store with app's reducer
+
+// put store and actions on the window
+window.store = store;
+window.addOrange = addOrange;
+window.addApple = addApple;
+
+store.getState();
+store.dispatch(addOrange);
+store.getState();
+store.dispatch(addApple);
+store.getState();
+const addLychee = { type: 'ADD_FRUIT', fruit: 'lychee' };
+store.dispatch(addLychee);
+store.getState();
