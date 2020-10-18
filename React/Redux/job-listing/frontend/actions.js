3e3b07a53b230372165a8e5764be2c5d22d5c6bb
Michael Stockermans <michael.stockermans@gmail.com>
2020-10-18 16:20:44 -0400

    job-listing: react/redux store app

---
+++ React/Redux/job-listing/frontend/actions.js
@@ -0,0 +1,11 @@
+const selectLocation = (city, jobs) => {
+  return {
+    type: 'SWITCH_LOCATION',
+    city: city,
+    jobs: jobs
+  };
+};
+
+export default selectLocation;
+
+window.selectLocation = selectLocation;
