0b83d16f57a71acde4568a651317f778c82a58bd
Michael Stockermans <michael.stockermans@gmail.com>
2020-10-19 17:52:13 -0400

    react-todos: implemented reducer, selector and action for steps

--- React/Redux/react-todos/frontend/reducers/selectors.js
+++ React/Redux/react-todos/frontend/reducers/selectors.js
@@ -1,7 +1,22 @@
 export const allTodos = (state) => {
+
   let theseTodoKeys = Object.keys(state.todos);
   let newTodoArray = theseTodoKeys.map((todoKey) => {
     return state.todos[todoKey];
   });
+
   return newTodoArray;
+};
+
+export const stepsByTodoId = (state, todoId) => {
+  let theseStepKeys = Object.keys(state.steps);
+  let theseSteps = [];
+
+  theseStepKeys.forEach((stepKey) => {
+    if (state.steps[stepKey].todo_id == todoId) {
+      theseSteps.push(state.steps[stepKey]);
+    }
+  });
+
+  return theseSteps;
 };
--- React/Redux/react-todos/frontend/todo_redux.jsx
+++ React/Redux/react-todos/frontend/todo_redux.jsx
@@ -4,13 +4,19 @@ import configureStore from './store/store.js';
 import Root from './components/root';
 
 import { receiveTodos, receiveTodo } from './actions/todo_actions';
-import { allTodos } from './reducers/selectors';
+import { receiveSteps, receiveStep, removeStep } from './actions/step_actions';
+
+import { allTodos, stepsByTodoId } from './reducers/selectors';
 
 const store = configureStore();
 window.store = store;
 window.receiveTodo = receiveTodo;
 window.receiveTodos = receiveTodos;
+window.receiveSteps = receiveSteps;
+window.receiveStep = receiveStep;
+window.removeStep = removeStep;
 window.allTodos = allTodos;
+window.stepsByTodoId = stepsByTodoId;
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
