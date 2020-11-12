6ce30035a6774f388f72b637acb56d9d99389af0
Michael Stockermans <michael.stockermans@gmail.com>
2020-11-12 11:18:46 -0500

    BenchBNB: added AuthRoute and preloaded state for currentUser

--- React/Frontend Auth/BenchBNB/frontend/bench_bnb.jsx
+++ React/Frontend Auth/BenchBNB/frontend/bench_bnb.jsx
@@ -8,13 +8,27 @@ import { login } from './actions/session_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  const store = configureStore();
+
+  window.SessionUtils = SessionUtils;
+  window.login = login;
+
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: { id: window.currentUser.id }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
 
   window.getState = store.getState;
   window.dispatch = store.dispatch;
-  window.SessionUtils = SessionUtils;
   window.dispatch = store.dispatch;
-  window.login = login;
 
   ReactDOM.render(<Root store={store}/>, root);
 });
